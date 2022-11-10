32e7b0147de96a5a1c1488cab898772d25cc267c
Tyler Griffith <griffitht16@gmail.com>
2022-11-10 15:46:52 -0600

    Created products model + Installed apps + Migrated app to mysql workbench

--- products_project/settings.py
+++ products_project/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -102,7 +104,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
